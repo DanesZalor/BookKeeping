7ed43c651ead3070fcdebaee6e8e3930940d8aab
Daniel James Dolor <estheryahe@gmail.com>
2022-06-21 08:40:39 +0800

    added POST@api/entries/post.php api endpoint. (unchecked and untested)

--- src/api/entries/post.php
+++ src/api/entries/post.php
@@ -4,52 +4,93 @@
 
 /**
  * @param body { entrydesc: str, dateoftransaction: timestamp, rows: array() }
- * sample: entrydesc="description" dateoftransaction="2001-09-12 00:01:23" rows:='[{"accounttitle":"Cash", "amount":-2000}, {"accounttitle":"Accounts Recievable", "amount":2000}]'
-
+ * sample: entrydesc="description" dateoftransaction="2001-09-12 00:01:23" rows:='[{"accounttitle":"Cash", "amount":-2000}, {"accounttitle":"Accounts Receivable", "amount":2000}]'
  */
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     
+    $errorResponse = array(
+        "Expected Parameters:"=> array(
+            "entrydesc" => "str",
+            "dateoftransaction" => "timestamp",
+            "rows" => array(
+                array(
+                    "accounttitle" => "str",
+                    "amount" => "float",
+                ),"...",
+            ),
+        ),
+    );
+
+    function isAccountTitleinDB($acc){
+        return sizeof(db_query("SELECT accountname FROM account WHERE accountname='$acc'")->fetchAll(PDO::FETCH_NUM)) >= 1;
+    }
+
+    /**
+     * rows validation
+     * must be atleast 2 rows
+     * each with accounttitle:str and amount:float
+     */
     function correctArrayShape($arg){
-        
+        global $errorResponse;
+
         if(is_array($arg) && sizeof($arg) >= 2){
+
+            $totalamount = 0;
 
             foreach($arg as $obj){
                 
+
                 if( 
-                    !is_string($obj->accounttitle) ||
-                    !is_string($obj->amount) == null
-                ) return false;
-                
-                //echo $obj;
+                    (!property_exists($obj, 'amount') || !property_exists($obj, 'accounttitle')) ||
+                    !is_string($obj->accounttitle) || !is_numeric($obj->amount)
+                ) {
+                    $errorResponse = array("ERROR"=>"rows[i] must follow shape:{accounttitle:str, amount:float}");
+                    return false;
+                } else if(!isAccountTitleinDB($obj->accounttitle)){
+                    $errorResponse = array("ERROR"=> "'$obj->accounttitle' is not a registered accounttitle in the database");
+                    return false;
+                }
+                    
+                $totalamount += $obj->amount;
             }
-            return true;
+            
+            
+            if($totalamount != 0) $errorResponse = array("ERROR"=>"SUM(rows.amount) should be == 0. Actual SUM: ${totalamount}");
+            
+            return $totalamount == 0;
         }
+
+        if(sizeof($arg)<2) $errorResponse = array("ERROR"=>"there should be atleast 2 rows");
+
         return false;
     }
 
-    // check param shape
     if(
         is_string($body->entrydesc) && 
         is_string($body->dateoftransaction) && 
         correctArrayShape($body->rows)
-    )
-        respond('ok',200);
-    else 
-        respond(
-            array(
-                "Expected Parameters:"=> array(
-                    "entrydesc" => "str",
-                    "dateoftransaction" => "timestamp",
-                    "rows" => array(
-                        array(
-                            "accounttitle" => "str",
-                            "amount" => "float",
-                        ),"..."
-                    ),
-                )
-        ),
-         406);
-}else{
-    respond("Invalid HTTP Method",405);
-}
+    ){
+
+        db_query("INSERT INTO  journalentry (dateoftransaction, entrysummary) VALUES ('$body->dateoftransaction', '$body->entrydesc')");
+        
+        $querystr = "SELECT id FROM journalentry WHERE dateoftransaction='$body->dateoftransaction' AND entrysummary='$body->entrydesc';";
+        
+        $jeid = db_query($querystr)->fetchAll(PDO::FETCH_NUM)[0][0];
+        
+        
+        foreach($body->rows as $row){
+            db_query("INSERT INTO jerow (accounttitle, amount, jeid) VALUES ('$row->accounttitle', $row->amount, ${jeid})");
+        }
+
+        respond(array(
+            "msg"=>"Successfully added journal entry",
+            "journalentry.id" => $jeid,
+        ),200);
+    }
+        
+    else // wrong parameters; specify 
+        respond($errorResponse,406);
+
+}else respond("Invalid HTTP Method",405);
+
 ?>
